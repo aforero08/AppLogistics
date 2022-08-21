2d32129fe44c1db4a3d98d58aa454f1707d881d5
Andres Forero <forero08@gmail.com>
2022-08-21 10:44:33 -0500

    Add novelties to Excel report and UI report. Added column number constants

---
+++ src/AppLogistics.Components/Extensions/Native/EnumerableExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AppLogistics.Components.Extensions.Native
+{
+    public static class EnumerableExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable) => enumerable == null || !enumerable.Any();
+    }
+}
