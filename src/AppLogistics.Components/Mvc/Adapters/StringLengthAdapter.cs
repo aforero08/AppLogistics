5777e5d3eb04479fad9f0893c43b2092d156f2fb
aforero08 <Forest@DESKTOP-JVEDIK9>
2025-10-07 23:01:14 -0400

    Validation failed: obsolete ModelMetadataIdentity.ForProperty overload usage and adapter test classes referencing internal MVC 2.x adapter types removed/changed in .NET 6. Need to refactor tests to new metadata API (use PropertyInfo-based overload) and update/replace custom adapter test implementations.

--- src/AppLogistics.Components/Mvc/Adapters/StringLengthAdapter.cs
+++ src/AppLogistics.Components/Mvc/Adapters/StringLengthAdapter.cs
@@ -1,4 +1,4 @@
-﻿using AppLogistics.Resources;
+using AppLogistics.Resources;
 using Microsoft.AspNetCore.Mvc.DataAnnotations.Internal;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
